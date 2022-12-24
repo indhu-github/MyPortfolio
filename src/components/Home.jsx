248af04d3871d3e329f41071cd2ab07b58d74cd8
indhu yelineni <indhusworkmail@gmail.com>
2022-12-23 21:33:09 -0500

    Social Icons

---
+++ src/components/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { HighArrowNarrowRight } from "react-icons/hi";
+
+const Home = () => {
+  return <div className="w-full h-screen bg-[#0a192f]">Home</div>;
+};
+
+export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React,{useState} from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
+import { FaBars, FaTimes, FaGithub, FaLinkedin} from "react-icons/fa";
+import {HiOutlineMail} from "react-icons/hi";
+import {HiDownload} from "react-icons/hi";
 
 function Navbar() {
 const [nav,setNav] = useState(false);
@@ -37,6 +39,25 @@ const handleClick = () =>{
         <li className="py-6 text-4xl">Work</li>
         <li className="py-6 text-4xl">Contact</li>
       </ul>
+
+        {/* Social Icons */}
+      <div className=" hidden lg:flex fixed flex-col top-[35%] left-0">
+        <ul>
+          <li className="w-[160px] h-[50px] ml-[-100px] flex justify-between items-center hover:ml-[-10px] duration-300 bg-blue-600">
+            <a className="flex justify-between items-center w-full text-gray-100" href="">LinkedIn <FaLinkedin size={30}/></a>
+          </li>
+          <li className="w-[160px] h-[50px] ml-[-100px] flex justify-between items-center hover:ml-[-10px] duration-300 bg-[#333333]">
+            <a className="flex justify-between items-center w-full text-gray-100" href="">Github <FaGithub size={30}/></a>
+          </li>
+          <li className="w-[160px] h-[50px] ml-[-100px] flex justify-between items-center hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
+            <a className="flex justify-between items-center w-full text-gray-100" href="">Email <HiOutlineMail size={30}/></a>
+          </li>
+          <li className="w-[160px] h-[50px] ml-[-100px] flex justify-between items-center hover:ml-[-10px] duration-300 bg-[#565f69]">
+            <a className="flex justify-between items-center w-full text-gray-100" href="">Resume <HiDownload size={30}/></a>
+          </li>
+        </ul>
+
+      </div>
     </div>
   );
 }
