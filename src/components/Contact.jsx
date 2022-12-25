cfa2a419361d81bcd6126d2ad548e48e0fe2f867
indhu yelineni <indhusworkmail@gmail.com>
2022-12-25 17:36:54 -0500

    Contact section done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
-    <div name="contact" className="w-full h-screen text-gray-300 bg-[#0a192f]">Contact</div>
-  )
-}
+    <div
+      name="contact"
+      className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4"
+    >
+      <form
+        method="POST"
+        action="https://getform.io/f/52cbac6e-3c1c-4551-862a-05c4bef0d74f"
+        className="flex flex-col max-w-[600px] w-full"
+      >
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">
+            Let's Connect
+          </p>
+        </div>
+        <input
+          className="bg-[#ccd6f6] p-2"
+          type="text"
+          placeholder="Name"
+          name="name"
+        />
+        <input
+          className="my-4 p-2 bg-[#ccd6f6]"
+          type="email"
+          placeholder="Email"
+          name="email"
+        />
+        <textarea
+          className="bg-[#ccd6f6] p-2"
+          name="message"
+          rows="10"
+          placeholder="Message"
+        ></textarea>
+        <button className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center">
+          Let's Collaborate
+        </button>
+      </form>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
