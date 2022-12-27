4398c43a5afeea16d8a3aea9f4db5e39192fa1ed
indhu yelineni <indhusworkmail@gmail.com>
2022-12-26 20:29:56 -0500

    ready

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className="w-full bg-[#ffffff] p-4">
-        <div className='text-black flex flex-col justify-center items-center'>
+        <div className='flex flex-col justify-center items-center'>
             <p className='font-semibold'>Designed and developed by</p>
-            <p className='text-[#070c14]'>Indhu Yelineni | +1(513)-(888)-(6884)</p>
+            <p className='text-[#37379c] font-semibold'>Indhu Yelineni | +1(513)-(888)-(6884)</p>
             <p>© All rights reserved | 2022</p>
         </div>
     </div>
